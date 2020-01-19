7e6e3afae9443fbd36b7222e90b0fbd597a7d17e
Ryan <1018718155@qq.com>
2020-01-19 11:58:26 +0800

    match_icon.py

--- image_detection/match_icon.py
+++ image_detection/match_icon.py
@@ -28,19 +28,19 @@ class Key_listener(PyKeyboardEvent):
         return False
 
 
-def save_icon(im1, im2):
+def save_icon(im0, im):
     for pos_name, rect in screen_icon_position.items():
-        x1, y1, x2, y2 = rect
+        y1, x1, y2, x2 = rect
         x1, y1 = x1 + 3, y1 + 3
         x2, y2 = x2 - 3, y2 - 3
-        icon1 = im1[y1:y2, x1:x2]
-        icon2 = im2[y1:y2, x1:x2]
-        icon_diff = abs(icon1 - icon2)
+        icon0 = im0[y1:y2, x1:x2]
+        icon = im[y1:y2, x1:x2]
+        icon_diff = abs(icon - icon0)
         icon_diff_byte = np.where(icon_diff == 0, 0, 1).astype(np.uint8)
         icon_diff_byte = icon_diff_byte[:, :, 0] & icon_diff_byte[:, :, 1] & icon_diff_byte[:, :, 2]
         if np.sum(icon_diff_byte) > min_icon_area:
             icon_same_byte = (1 - icon_diff_byte)[:, :, np.newaxis]
-            icon_3c = icon1 * icon_same_byte
+            icon_3c = icon * icon_same_byte
             icon_name = get_icon_name(icon_3c)
             icon_4c = np.concatenate((icon_3c, icon_same_byte * 255), axis=-1)
             cv2.imwrite('icons/' + pos_name + '-' + icon_name + '.png', icon_4c)
@@ -52,7 +52,9 @@ def get_icon_name(icon):
 
 
 if __name__ == '__main__':
+    kl = Key_listener()
+    kl.run()
+
     im1 = cv2.imread('test_screens_icon_match/0.png')
     im2 = cv2.imread('test_screens_icon_match/1.png')
-
     save_icon(im1, im2)
