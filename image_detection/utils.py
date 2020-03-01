78ceb2fdc1e110485b4da16588e59f5f74d04cd4
Shuai Yan <1018718155@qq.com>
2020-03-01 13:20:49 +0800

    clear

--- image_detection/utils.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def get_white_shield(im, min_rgb):
-    shield_rgb = np.where(im > min_rgb, 255, 0)
-    shield = shield_rgb[:, :, 0] & shield_rgb[:, :, 1] & shield_rgb[:, :, 2]
-    return shield
